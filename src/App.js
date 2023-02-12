cb7fb2e2dbf6b11b3cbaec4139d34de0fa8df89f
Swarga-codes <sd3526@srmist.edu.in>
2023-02-12 15:47:08 +0530

    added functionality to search for the temp of any city

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import {useEffect, useState} from 'react';
 
 function App() {
+  const[loader,setLoader]=useState(true);
  const[latitude,setLatitude]=useState();
  const[longitude,setLongitude]=useState();
  const[city, setCity] = useState('');
@@ -17,51 +18,60 @@ function App() {
   console.log(position.coords);
   setLatitude(position.coords.latitude);
   setLongitude(position.coords.longitude);
+  //fetching the details about the current city
   const geoUrl = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`
 fetch(geoUrl)
 .then(res => res.json())
 .then(data =>{
  console.log(data);
+ setLoader(false);
  setCity(data.city);
  setState(data.principalSubdivision)
 })
 });
  });
 const weatherApiCurrent = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m`
-  const getWeatherCurrent = () => fetch(weatherApiCurrent)
+//fetching the weather of the current city 
+const getWeatherCurrent = () => fetch(weatherApiCurrent)
   .then(res => res.json())
   .then(data => {
     console.log(data);
-    setcurrentTemp(data.current_weather.temperature);
+    setcurrentTemp(data.current_weather.temperature+"°C");
   
   });
   const weatherApiCustom = `http://api.openweathermap.org/geo/1.0/direct?q=${inpCity}&limit=1&appid=b82efa1d95b00bf3c54041947d07a6d4`;
-const getWeatherCustom = async() => fetch(weatherApiCustom)
+  const weatherApiTemp = `https://api.open-meteo.com/v1/forecast?latitude=${customLat}&longitude=${customLong}&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m`
+  //fetching the latitude and longitude of the given city
+  const getWeatherCustom = async() => fetch(weatherApiCustom)
 .then(res => res.json())
 .then(data => {
   console.log(data);
   setCustomLat(data[0].lat);
   setCustomLong(data[0].lon);
- 
-})
-const weatherApiTemp = `https://api.open-meteo.com/v1/forecast?latitude=${customLat}&longitude=${customLong}&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m`
-  const getWeatherInDegrees = () => fetch(weatherApiTemp)
+  //fetching the weather using the latitude and longitude
+  fetch(weatherApiTemp)
   .then(res => res.json())
   .then(data => {
-    console.log(data.current_weather.temperature);
+    console.log(data);
     setCustomTemp(data.current_weather.temperature+"°C");
   
   });
+})
+
+  // const getWeatherInDegrees = () => 
   return (
     <div className="App">
-    <h1>Current city</h1>
+    <h1>Weather App</h1>
+    <p>by</p>
+    <h4>Markus</h4>
+    <h2>Current city</h2>
+    {loader? <h3>Loading...</h3>:
     <h3>Place : {city}, {state}</h3>
+  }
   <button onClick={getWeatherCurrent}>Fetch weather for current city</button>
-  <h1>Temperature: {currentTemp} °C</h1>
+  <h1>Temperature: {currentTemp}</h1>
   <input type="text" placeholder='Enter the city' onChange={(e)=>setinpCity(e.target.value)}/>
   <button onClick={getWeatherCustom}>Find!</button>
-  <p>{customLat} || {customLong}</p>
-  <button onClick={getWeatherInDegrees}>Click</button>
   <h3>Temp: {customTemp}</h3>
      </div>
    
